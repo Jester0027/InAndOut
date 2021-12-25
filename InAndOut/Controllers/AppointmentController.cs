5aa94a2d882674441dd69741bef2c17592461a73
Jester0027 <paul.ned@outlook.com>
2021-12-25 14:06:54 +0100

    Added SQL Server db context

---
+++ InAndOut/Controllers/AppointmentController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace InAndOut.Controllers
+{
+    [Route("/appointments")]
+    public class AppointmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
