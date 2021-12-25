694be80352b8b0496656719c3e9761c342c67eb3
Jester0027 <paul.ned@outlook.com>
2021-12-25 15:00:02 +0100

    List items view

---
+++ InAndOut/Controllers/ItemController.cs
@@ -0,0 +1,26 @@
+﻿using InAndOut.Data;
+using InAndOut.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace InAndOut.Controllers
+{
+    [Route("items")]
+    public class ItemController : Controller
+    {
+
+        private readonly ApplicationDbContext _dbContext;
+
+        public ItemController(ApplicationDbContext db)
+        {
+            _dbContext = db;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            IEnumerable<Item> items = _dbContext.Items;
+            return View(items);
+        }
+    }
+}
